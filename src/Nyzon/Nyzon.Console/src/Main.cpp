c1ca4e21332a9285ea2acf7e4fa813480ff00f49
Tomas Wegenast <31850785+Tomi-15@users.noreply.github.com>
2018-10-27 13:12:47 -0300

    Removed c++ projects

--- src/Nyzon/Nyzon.Console/src/Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Logging/Logging.h"
-
-// Application entry point
-int main()
-{
-	// Keep console opened
-	std::cin.get();
-}
--- src/Nyzon/Nyzon.Console/src/datetime/datetime.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "..\include_headers.h"
-
-// Get current date and time and returns a string with format
-const std::string currentDateTime();
